73442051ff52865c77b9481a8293d5599d15bdfb
Boris Zhukov <bdzhukov@edu.hse.ru>
2026-09-16 12:59:50 +0300

    fix rebase artefacts

--- src_np/gmm/__init__.py
+++ src_np/gmm/__init__.py
@@ -1,15 +1,3 @@
 from .moments import MomentGaussianMixtureModel
-<<<<<<< HEAD
-from .one_s import OneSGaussianMixtureModel
-from .smooth_em import SmoothEMGaussianMixtureModel
-
-__all__ = [
-    "MomentGaussianMixtureModel",
-    "MultiSGaussianMixtureModel",
-    "OneSGaussianMixtureModel",
-    "SmoothEMGaussianMixtureModel",
-]
-=======
 
 __all__ = ["MomentGaussianMixtureModel"]
->>>>>>> f010971 (refactoring + add entropy regularization)
